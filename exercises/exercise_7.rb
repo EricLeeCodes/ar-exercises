b15bb2738164d3506c2d84a34edba3d2ba3ef89b
Eric Lee <ericleecodes@gmail.com>
2024-04-02 00:10:47 -0700

    Add validation for databases and test while displaying errors if requirements not met

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,15 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+puts "Name the store you would like created:"
+user_store_name = gets.chomp
+
+user_store = Store.create(name: "#{user_store_name}")
+
+if user_store.valid?
+  user_store.save
+else
+puts user_store.errors.full_messages
+end
